086c195f2e5e52bbba3a4f39e79c4d5ca7f5a44a
Benjamin Jorand <benjamin.jorand@botsunit.com>
2019-12-14 01:08:55 +0100

    list implm

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/bjorand/nombda/engine"
@@ -29,6 +30,19 @@ func Base() gin.HandlerFunc {
 	}
 }
 
+func isAuthenticated(c *gin.Context) bool {
+	t := tokenHeader{}
+	if err := c.ShouldBindHeader(&t); err != nil {
+		c.JSON(500, err)
+		return false
+	}
+	if t.AuthToken != token {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		return false
+	}
+	return true
+}
+
 func main() {
 	flag.StringVar(&listenAddr, "listen-addr", ":8080", "server listen address")
 	flag.Parse()
@@ -47,16 +61,31 @@ func main() {
 	router.Use(gin.Recovery())
 	router.Use(Base())
 
+	router.GET("/hooks/", func(c *gin.Context) {
+		if !isAuthenticated(c) {
+			return
+		}
+		hooksFilename, err := filepath.Glob(configDir + "/*/*.yml")
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		var hooksName []string
+
+		for _, hookFilename := range hooksFilename {
+			hookName := filepath.Base(hookFilename)
+			hookName = strings.TrimRight(hookName, ".yml")
+			hooksName = append(hooksName, hookName)
+
+		}
+		c.JSON(http.StatusOK, gin.H{"hooks": hooksName})
+	})
+
 	router.POST("/hooks/:id/:action", func(c *gin.Context) {
-		t := tokenHeader{}
-		if err := c.ShouldBindHeader(&t); err != nil {
-			c.JSON(500, err)
+		if !isAuthenticated(c) {
 			return
 		}
-		if t.AuthToken != token {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-			return
-		}
+
 		hook, err := engine.ReadHook(configDir, c.Param("id"), c.Param("action"))
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
